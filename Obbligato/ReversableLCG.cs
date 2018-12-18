81cf30530d8097ac830f1e0982ca9cf66cd73bc2
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-12-18 13:30:07 -0500

    Added b89 Test framework

---
+++ Obbligato/ReversableLCG.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace Obbligato
+{
+    public class ReversableLCG
+    {
+        private uint modulus; //m
+        private uint multiplier; // a
+        private uint increment; //c
+        private uint seed; //x
+        private ReversableLCG instance; 
+        
+
+        public ReversableLCG(uint seed)
+        {
+            this.modulus = modulus;
+            this.multiplier = multiplier;
+            this.increment = increment;
+            this.seed = seed;
+            VerifyFields();
+        }
+
+        private void VerifyFields()
+        {
+            byte verified = 0;
+            bool multiplierFlag = false;
+            bool incrementFlag = false;
+
+            if (multiplier < modulus)
+            {
+                verified++;
+            }
+            else
+            {
+                multiplierFlag = true;
+            }
+
+            if (multiplier == 0)
+            {
+                verified++;
+            }
+            else
+            {
+                multiplierFlag = true;
+            }
+
+            if (increment < modulus)
+            {
+                verified++;
+            }
+            else
+            {
+                incrementFlag = true;
+            }
+
+            if (verified != 3)
+            {
+                instance = null;
+            }
+        }
+
+    }
+}
